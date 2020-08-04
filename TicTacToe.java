57302956e500e0669072a2ab44bda5cf95425ee9
shashank srivastava <shashank.1696@gmail.com>
2020-08-04 21:25:57 +0530

    ADDED: Method used to display the game board and also to refresh the …

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,16 +3,17 @@ public class TicTacToe {
 	public static void main(String[] args) {
 
 		TicTacToe tictac = new TicTacToe();
+		System.out.println("Welcome to TicTacToe game!!!");
 		tictac.printGameBoard();
 	}
 
 	//Here the function printGameBoard is used to print gameBoard aur used to refresh the Board
 	private void printGameBoard() {
-		char [] [] gameBoard = {{ ' ' , ' ' ,'|' , ' ' , ' ' , '|' , ' ' , ' ' },
-      				     { '-' ,  '-' , '+' , '-' ,   '-' , '+' ,'-' , '-' },
-				     { ' ' , ' ' ,'|' , ' ' , ' ' , '|' , ' ' , ' ' },
-				     { '-' ,  '-' , '+' , '-' ,   '-' , '+' ,'-' , '-' },
-				     { ' ' , ' ' ,'|' , ' ' , ' ' , '|' , ' ' , ' '}};
+		char [] [] gameBoard = {{' ','|',' ','|',' '},
+					{'-','+','-','+','-'},
+					{' ','|',' ','|',' '},
+					{'-','+','-','+','-'},
+					{' ','|',' ','|',' '}};
 
 		for (char [] row : gameBoard) {
 			for(char c : row) {
